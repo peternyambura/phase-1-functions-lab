10212e5eb7535efcd235b7e4e3734f2d7629c833
= <peterndirangu0005@gmail.com>
2023-07-12 17:53:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    if (pickupLocation >= 42) {
+      return pickupLocation - 42;
+    } else {
+      return 42 - pickupLocation;
+    }
+  }
+
+  function distanceFromHqInFeet(pickupLocation) {
+    let blocks = distanceFromHqInBlocks(pickupLocation);
+    return blocks * 264;
+  }
+  
+  function distanceTravelledInFeet(start, destination) {
+    let blocks = Math.abs(destination - start);
+    return blocks * 264;
+  }
+  
+  function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination);
+  
+    if (distance <= 400) {
+      return 0;
+    } else if (distance > 400 && distance <= 2000) {
+      return (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance <= 2500) {
+      return 25;
+    } else {
+      return 'cannot travel that far';
+    }
+  }
+  
